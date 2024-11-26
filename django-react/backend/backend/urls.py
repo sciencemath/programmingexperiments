68ea7ffa3a84ae953073bdd910e3dcf7b4820abb
sciencemath <matrixmachinelearning@gmail.com>
2024-11-25 23:28:55 -0500

    started react django application with auth

---
+++ django-react/backend/backend/urls.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from django.urls import path
+from api.views import CreateUserView
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+]
