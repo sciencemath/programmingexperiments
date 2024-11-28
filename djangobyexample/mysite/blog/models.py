f7e95a81cab5d806cfc1b032fee0aa9352a711c4
sciencemath <matrixmachinelearning@gmail.com>
2024-11-28 09:42:58 -0500

    commenting system, pagination, and sending email via SMTP, setting up environment vars

--- djangobyexample/mysite/blog/models.py
+++ djangobyexample/mysite/blog/models.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.db import models
+from django.urls import reverse
 from django.utils import timezone
 # uses the database server's current date and time
 # from django.db.models.functions import Now
@@ -15,7 +16,7 @@ class Post(models.Model):
     DRAFT = 'DF', 'Draft'
     PUBLISHED = 'PB', 'Published'
   title = models.CharField(max_length=250)
-  slug = models.SlugField(max_length=250)
+  slug = models.SlugField(max_length=250, unique_for_date='publish')
   author = models.ForeignKey(
     settings.AUTH_USER_MODEL,
     on_delete=models.CASCADE,
@@ -42,3 +43,36 @@ class Post(models.Model):
 
   def __str__(self):
     return self.title
+  
+  def get_absolute_url(self):
+    return reverse(
+      'blog:post_detail',
+      args=[
+        self.publish.year,
+        self.publish.month,
+        self.publish.day,
+        self.slug
+      ]
+    )
+
+class Comment(models.Model):
+  post = models.ForeignKey(
+    Post,
+    on_delete=models.CASCADE,
+    related_name='comments'
+  )
+  name = models.CharField(max_length=80)
+  email = models.EmailField()
+  body = models.TextField()
+  created = models.DateTimeField(auto_now_add=True)
+  updated = models.DateTimeField(auto_now=True)
+  active = models.BooleanField(default=True)
+
+  class Meta:
+    ordering = ['created']
+    indexes = [
+      models.Index(fields=['created']),
+    ]
+  
+  def  __str__(self):
+    return f'Comment by {self.name} on {self.post}'
