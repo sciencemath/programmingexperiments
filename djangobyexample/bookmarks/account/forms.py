3f8c70fee48fdd4b003b8757de1e0199b9badfd8
sciencemath <matrixmachinelearning@gmail.com>
2024-11-30 16:55:42 -0500

    adding google oauth2.0, implementing the messages in installed apps, adding https

--- djangobyexample/bookmarks/account/forms.py
+++ djangobyexample/bookmarks/account/forms.py
@@ -25,11 +25,26 @@ class UserRegistrationForm(forms.ModelForm):
     if cd['password'] != cd['password2']:
       raise forms.ValidationError('Passwords don\'t match')
     return cd['password2']
+  
+  def clean_email(self):
+    data = self.cleaned_data['email']
+    if get_user_model().objects.filter(email=data).exists():
+      raise forms.ValidationError('Email already in use.')
+    return data
 
 class UserEditForm(forms.ModelForm):
   class Meta:
     model = get_user_model()
     fields = ['first_name', 'last_name', 'email']
+
+  def clean_email(self):
+    data = self.cleaned_data['email']
+    qs = get_user_model().objects.exclude(
+      id=self.instance.id
+    ).filter(email=data)
+    if qs.exists():
+      raise forms.ValidationError('Email already in use.')
+    return data
 
 class ProfileEditForm(forms.ModelForm):
   class Meta:
--- djangobyexample/bookmarks/bookmarks/settings.py
+++ djangobyexample/bookmarks/bookmarks/settings.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -13,7 +14,8 @@ SECRET_KEY = 'django-insecure-wi%@x=a5su!#d838sit35ok$dblewtbyh3tz=cku5al!h2z&k2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+# edited in /etc/hosts
+ALLOWED_HOSTS = ['mysite.com', 'localhost', '127.0.0.1']
 
 LOGIN_REDIRECT_URL = 'dashboard'
 LOGIN_URL = 'login'
@@ -23,6 +25,22 @@ EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+SOCIAL_AUTH_GOOGLE_OAUTH2_KEY = config('GOOGLE_OAUTH2_KEY')
+SOCIAL_AUTH_GOOGLE_OAUTH2_SECRET = config('GOOGLE_OAUTH2_SECRET')
+
+SOCIAL_AUTH_PIPELINE = [
+  'social_core.pipeline.social_auth.social_details',
+  'social_core.pipeline.social_auth.social_uid',
+  'social_core.pipeline.social_auth.auth_allowed',
+  'social_core.pipeline.social_auth.social_user',
+  'social_core.pipeline.user.get_username',
+  'social_core.pipeline.user.create_user',
+  'account.authentication.create_profile',
+  'social_core.pipeline.social_auth.associate_user',
+  'social_core.pipeline.social_auth.load_extra_data',
+  'social_core.pipeline.user.user_details',
+]
 
 # Application definition
 
@@ -34,6 +52,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'social_django',
+    'django_extensions',
+]
+
+# users can login using username or email
+AUTHENTICATION_BACKENDS = [
+  'django.contrib.auth.backends.ModelBackend',
+  'account.authentication.EmailAuthBackend',
+  'social_core.backends.google.GoogleOAuth2',
 ]
 
 MIDDLEWARE = [
