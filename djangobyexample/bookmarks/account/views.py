935d743bbe855735b23cb6afa390d1c89a93d3e1
sciencemath <matrixmachinelearning@gmail.com>
2024-11-30 09:54:37 -0500

    implementing a auth system in django, a password reset/change, and an image uploader

---
+++ djangobyexample/bookmarks/account/views.py
@@ -0,0 +1,94 @@
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+from django.shortcuts import render
+from .forms import (
+  LoginForm,
+  UserRegistrationForm,
+  UserEditForm,
+  ProfileEditForm
+)
+from .models import Profile
+
+@login_required
+def dashboard(request):
+  return render(
+    request,
+    'account/dashboard.html',
+    {'section': 'dashboard'}
+  )
+
+def user_login(request):
+  if request.method == 'POST':
+    form = LoginForm(request.POST)
+    if form.is_valid():
+      cd = form.cleaned_data
+      user = authenticate(
+        request,
+        username=cd['username'],
+        password=cd['password']
+      )
+      if user is not None:
+        if user.is_active:
+          login(request, user) # sets the session
+          return HttpResponse('Authenticated successfully')
+        else:
+          return HttpResponse('Disabled account')
+      else:
+        return HttpResponse('Invalid login')
+  else:
+    form = LoginForm()
+  return render(request, 'account/login.html', {'form': form})
+
+def register(request):
+  if request.method == 'POST':
+    user_form = UserRegistrationForm(request.POST)
+    if user_form.is_valid():
+      new_user = user_form.save(commit=False)
+      # set_password() will handle the hashing
+      new_user.set_password(
+        user_form.cleaned_data['password']
+      )
+      new_user.save()
+      # this will associate a Profile with a user, although if a user is created
+      # via admin this won't be associated we must use signals.
+      Profile.objects.create(user=new_user)
+      return render(
+        request,
+        'account/register_done.html',
+        {'new_user': new_user}
+      )
+  else:
+    user_form = UserRegistrationForm()
+  return render(
+    request,
+    'account/register.html',
+    {'user_form': user_form}
+  )
+
+@login_required
+def edit(request):
+  if request.method == 'POST':
+    user_form = UserEditForm(
+      instance=request.user,
+      data=request.POST
+    )
+    profile_form = ProfileEditForm(
+      instance=request.user.profile,
+      data=request.POST,
+      files=request.FILES
+    )
+    if user_form.is_valid() and profile_form.is_valid():
+      user_form.save()
+      profile_form.save()
+  else:
+    user_form = UserEditForm(instance=request.user)
+    profile_form = ProfileEditForm(instance=request.user.profile)
+  return render(
+    request,
+    'account/edit.html',
+    {
+      'user_form': user_form,
+      'profile_form': profile_form
+    }
+  )
